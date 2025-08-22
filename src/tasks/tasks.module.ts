8e1609ea21c99cb06fb515f4b64cdf4f5a9d6e52
bienvenue cedrick <cedrick4smith@gmail.com>
2025-08-22 22:51:40 +0200

    feat: update tasks module to include task ownership guard

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Task } from './entities/task.entity';
 import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
+import { TaskOwnershipGuard } from '../auth/guards/task-ownership.guard';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Task])],
   controllers: [TasksController],
-  providers: [TasksService],
+  providers: [TasksService, TaskOwnershipGuard],
 })
 export class TasksModule {}
