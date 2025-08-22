80609da9239072c3f4a9f79583366739c82f3f92
bienvenue cedrick <cedrick4smith@gmail.com>
2025-08-22 22:51:40 +0200

    feat: add auth module to app module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
 import { TasksModule } from './tasks/tasks.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule, UsersModule, TasksModule],
+  imports: [DatabaseModule, UsersModule, TasksModule, AuthModule],
 })
 export class AppModule {}
