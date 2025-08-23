8daaddfb0b10784429f494c763d0be5e5f7f01a4
bienvenue cedrick <cedrick4smith@gmail.com>
2025-08-23 09:20:09 +0200

    feat: add global ConfigModule to fix environment variable access
       - Import ConfigModule.forRoot with isGlobal: true
       - Fixes JWT authentication by making environment variables available
       - Resolves Unauthorized errors in protected endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule, UsersModule, TasksModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DatabaseModule, 
+    UsersModule, 
+    TasksModule, 
+    AuthModule
+  ],
 })
 export class AppModule {}
